99cd38418ef0590ce48f1dcc2eee8a97cd6b040c
quimicoquindio <dadiazm@uqvirtual.edu.co>
2020-12-05 21:04:27 -0500

    Primera versión del footer

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
   <div id="app">
+    <page-footer></page-footer>
+    <h1>{{título}}</h1>
   </div>
 </template>
 
 <script>
 
+import PageFooter from './components/PageFooter.vue'
+
 export default {
   name: 'App',
+  components:{
+    PageFooter
+  },
+  data(){
+    return{
+      título : 'Vivo dentro de VUE'
+    }
+  }
 }
 </script>
 
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer class="page-footer bg-secondary text-white pt-4">
+        <div class="container-fluid text-justify text-md-left">
+
+            <div class="row">
+                <div class="col-md-5 mt-md-0 mt-3">
+                    <h4 class="footer-title">Arquitectura de Servicios Tecnológicos</h4>
+                    <p>Proyecto motivado por la MisionTIC2022 en su ciclo 3, donde se utilizan lenguajes para una aplicación web, conceptos de HTML, CSS, JavaScript, el uso de APIs, de VUE-JS entre otras herramientas para el aprendizaje del desarrollo web.</p>
+                    <a href="https://github.com/jsreyl/semana-2-18.git">Enlace a nuestro repositorio!</a>
+                </div>
+                <div class="col-md-7 mt-md-4 mt-3">
+                    <h4 class="footer-title">Contacto:</h4>
+                    <ul class="footer-integrantes__lista">
+                        <li class="footer-integrantes__lista__elemento"> <i>Alejandro Díaz Moscoso</i> (<a href="alejo2dm@hotmail.com">Enviar E-mail</a>) - Arquitecto de Software</li>
+                        <li class="footer-integrantes__lista__elemento"> <i>Martha Isabel Betancourt Castañeda</i> (<a href="marticaisa4@gmail.com">Enviar E-mail</a>) - Analista de software</li>
+                        <li class="footer-integrantes__lista__elemento"> <i>Juan Sebastián Rey Lancheros</i> (<a href="juansreyl@gmail.com">Enviar E-mail</a>) - Desarrollador Fullstack</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'PageFooter',
+}
+</script>
+
+<style scoped>
+/* Esta etiqueta no se va a usar porque estamos usando los de Bootstrap */
+</style>
