4cc4c2fadae2dc67e1ee677833dc495e1338224e
quimicoquindio <dadiazm@uqvirtual.edu.co>
2020-12-05 22:41:25 -0500

    Estructura footer y equipo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-5">
+        <div class="col-md-4 mt-5" v-for="(item, index) of equipo" :key="index">
+          <team-card v.bind:member="item"></team-card>
+        </div>
+      </div>
+    </div>
     <page-footer></page-footer>
     <h1>{{título}}</h1>
   </div>
@@ -8,15 +15,40 @@
 <script>
 
 import PageFooter from './components/PageFooter.vue'
+import TeamCard from './components/TeamCard.vue'
 
 export default {
   name: 'App',
   components:{
-    PageFooter
+    PageFooter,
+    TeamCard
   },
   data(){
     return{
-      título : 'Vivo dentro de VUE'
+      título : 'Vivo dentro de VUE',
+      equipo: [
+        {
+          codigo: 1,
+          nombre: "Alejandro Díaz Moscoso",
+          descripcion: "Vive en Armenia, Quindío. Este joven de 27 años es químico de la Universidad del Quindío, y Magíster en Ciencias-Química de la Universidad de los Andes. Entre sus pasatiempos están: Mirar series/películas (ahora con el curso MisiónTIC2022 ni tiempo tiene); hacer cursos virtuales (MisiónTIC2022, Cambio Climático y Huella de Carbono, Diplomado en Excel que consta de 4 cursos, y dos idiomas); ir a cine; salir a tomar café (solo o con amigos); comer ensalada de frutas; y de vez en cuando salir a rumbear. No le gusta tomar. Actualmente está criando un polluelo llamado Duke",
+          rol: "Arquitecto de Software",
+          image: "./imagenes/alejo.jpg",
+        },
+        {
+          codigo: 2,
+          nombre: "Juan Sebastián Rey Lancheros",
+          descripcion: "Vive en Bogotá D.C. Este joven de 22 años es físico de la Universidad Nacional de Colombia. Entre sus pasatiempos están: Leer (de todo un poco porque gusta de variar géneros, pero sus favoritos son los libros de fantasía, tipo 'El señor de los anillos'); bailar; y jugar videojuegos (¿y a quién no?). Aunque parezca tímido, es un muchacho tranquilo y muy empático. Su mascota es una linda perrita llamada Totó.",
+          rol: "Desarrollador Fullstack",
+          image: "./imagenes/juan.jpg",
+        },
+        {
+          codigo: 3,
+          nombre: "Martha Isabel Betancourt Castañeda",
+          descripcion: "Vive en Popayán, Cauca. Esta joven es ingeniera de sistemas de la Corporación universitaria Remington. Entre sus pasatiempos están: Estudiar y aprender. Es una persona activa, trabajadora y siempre le pone la mejor actitud a cada situación que se le presenta. Actualmente trabaja de noche por lo que valora mucho la oportunidad de estudiar en el día.",
+          rol: "Analista de software",
+          image: "./imagenes/martha.jpg",
+        },
+      ],
     }
   }
 }
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,6 +1,27 @@
 <template>
-    <div>
+    <div class="card text-white bg-dark">
+        <div class="d-flex justify-content-center p-2">
+            <svg src="./imagenes/martha.jpg" height="300px" alt="Participante 3 Martha">
+            </svg>
+        </div>
+        <div class="card-body">
+            <h2 class="card-title">Martha Isabel Betancourt Castañeda</h2>
 
+            <p><b>Domicilio:</b> Popayán, Cauca</p>
+            <p><b>Estudios:</b>  Esta joven es ingeniera de sistemas de la Corporación universitaria Remington.</p>
+            <p><b>Pasatiempos:</b></p>
+                <nav>
+                    <ul>
+                        <li>Estudiar.</li>
+                        <li>Aprender.</li>
+                    </ul>
+                </nav>
+            <p><b>Datos curiosos:</b> Es una persona activa, trabajadora y siempre le pone la mejor actitud a cada situación que se le presenta. Actualmente trabaja de noche por lo que valora mucho la oportunidad de estudiar en el día.</p>
+            <p><b>E-mail:</b> marticaisa4@gmail.com</p>
+        </div>
+        <pre>
+            {{ member }}
+        </pre>
     </div>
 </template>
 
